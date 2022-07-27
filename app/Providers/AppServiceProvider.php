02e071b45c8ca1d1cbe72e1aadfb78ecb3703a86
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-27 14:40:32 +0430

    - // Fix SQLSTATE[42000] Exception bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /*
+        * set default value for string due to Illuminate\Database\QueryException : SQLSTATE[42000] Exception
+        * read more at:
+        * https://stackoverflow.com/questions/52623867/illuminate-database-queryexception-sqlstate42000
+        */
+        Schema::defaultStringLength(191);
     }
 }
