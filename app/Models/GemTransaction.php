b3ef2dac93c9b2f21809e1cd050b1ba8d2b4e011
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-27 14:39:45 +0430

    - // Add GemTransaction -> Gem relationship

--- app/Models/GemTransaction.php
+++ app/Models/GemTransaction.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class GemTransaction extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'sing' => 'boolean',
+    ];
+     /**
+     * Get the gem that owns the gemTransaction.
+     */
+    public function gem()
+    {
+        return $this->belongsTo(Gem::class);
+    }
 }
