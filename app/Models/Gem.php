b35bdb0f3a35c9bf887b038f5236c0252beabc20
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-27 14:32:55 +0430

    - // Add relationships, Gem -> User relationship Gem -> GemTransaction relationship

--- app/Models/Gem.php
+++ app/Models/Gem.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Gem extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+
+    ];
+    /**
+     * Get the user that owns the gem.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    /**
+     * Get the transactions for the gem.
+     */
+    public function transactions()
+    {
+        return $this->hasMany(GemTransaction::class);
+    }
 }
