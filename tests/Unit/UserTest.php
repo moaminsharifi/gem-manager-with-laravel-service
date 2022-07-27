f039bf0bfd8f4f51a9f2d075c01e47e2a1931a92
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-07-27 14:44:51 +0430

    - // Add UserTest Add users database has expected columns test Add a user has a gem test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -4,9 +4,10 @@ namespace Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\User;
+use App\Models\Gem;
 class UserTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
@@ -21,7 +22,33 @@ class UserTest extends TestCase
     {
         $this->assertTrue( 
             Schema::hasColumns('users', [
-              'id','name', 'email', 'email_verified_at', 'password'
-          ]), 1);
+                'id','name', 'email', 'email_verified_at', 'password', 'remember_token', 'created_at' , 'updated_at'
+            ]), 1);
     }
+
+    /**
+     * test a user has a gem
+     * @test
+     * @group Unit
+     * @group User
+     * @return void
+     */
+    public function a_user_has_a_gem()
+    {
+        $user = User::factory()->create(); 
+        $gem = Gem::factory()->create(['user_id' => $user->id]); 
+
+        // check user have gem which is instance of Gem
+        $this->assertInstanceOf(Gem::class, $user->gem); 
+        
+        // check user has one gem
+        $this->assertEquals(1, $user->gem->count()); 
+
+    }
+
+    
+    
+
+
+    
 }
